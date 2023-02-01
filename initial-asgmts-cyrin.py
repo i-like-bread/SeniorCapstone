9c8c16ace4c40204eeb2bf54d7c4f398867a7ebd
i-like-bread <72410972+i-like-bread@users.noreply.github.com>
2023-02-01 14:25:31 -0800

    Update initial-asgmts-cyrin.py

--- initial-asgmts-cyrin.py
+++ initial-asgmts-cyrin.py
@@ -27,8 +27,9 @@ time.sleep(5)
 
 # if you are not already in a continuation of the exercise,
 # start a new exercise
-# doesn't actually work??
-if driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]') is True:
+# doesn't actually work?? 
+# theres an iframe in every page so uhhh
+if driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]'):
     #find CYRIN iframe and switch to it
     iframe = driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]')
     driver.switch_to.frame(iframe)
@@ -40,17 +41,33 @@ if driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]') is True:
 # wait for the vm to load
 time.sleep(10)
 
-# switch from Computer C to Computer A (outside of the vm)
-# work in progress
-select = Select(driver.find_element(By.XPATH, '//*[@id="computersMenuButton"]'))
-select.select_by_visible_text('ComputerA')
+# switch from Computer C to Computer A (outside of the vm):
+#find CYRIN iframe and switch to it
+iframe = driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]')
+driver.switch_to.frame(iframe)
+
+driver.find_element(By.XPATH, '//*[@id="computersMenuButton"]').click()
+driver.find_element(By.XPATH, '//*[@id="displaymachine1_Ubuntu2004Desktop-4000-0182-eba4edce-809a-3fe20a37e1aa"]').click()
+
+
+
+# '//*[@id="computersMenuButton"]')).select_by_visible_text('ComputerA')
+
+
+time.sleep(10)
+
+
 
 # inside the vm: open the terminal
 
 
 
-# end the exercise
-driver.switch_to.frame(driver.find_element(By.XPATH, '//iframe[@id = "cyrinFrame"]'))
+
+#switch back to original iframe code
+# driver.switch_to.default_content()
+
+# end the exercise via button in iframe
+# driver.switch_to.frame(driver.find_element(By.XPATH, '//iframe[@id = "cyrinFrame"]'))
 driver.find_element(By.XPATH, "//button[@id = 'btnEndExercise']").click()
 time.sleep(5)
 driver.find_element(By.XPATH, "//button[@id = 'btnConfirmEndExercise']").click()
