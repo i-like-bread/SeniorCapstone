c7791d5e7c2ab59453409fc77b48892ccf133afa
i-like-bread <72410972+i-like-bread@users.noreply.github.com>
2023-02-01 16:56:33 -0800

    Update initial-asgmts-cyrin.py

--- initial-asgmts-cyrin.py
+++ initial-asgmts-cyrin.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from getpass import getpass
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 # prompt for username and password
 username = input("Username: ")
@@ -31,33 +32,32 @@ driver.switch_to.frame(iframe)
 
 # if the exercise hasn't started, checked for by the Training Scenario heading,
 # throwing errors here
-if driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/h2'):
-    #find the start exercise button and click
-    driver.find_element(By.XPATH, '//*[@id="launch-actions"]/form/input[4]').click()
-    #switch back to original iframe
-    driver.switch_to.default_content()
+try:
+    if driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/h2'):
+        #find the start exercise button and click
+        driver.find_element(By.XPATH, '//*[@id="launch-actions"]/form/input[4]').click()
+        #switch back to original iframe
+        # driver.switch_to.default_content()
+except NoSuchElementException:
+    pass
 
-# wait for the vm to load
-time.sleep(60)
+
 
 # switch from Computer C to Computer A (outside of the vm):
 #find CYRIN iframe and switch to it
-iframe = driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]')
-driver.switch_to.frame(iframe)
+# iframe = driver.find_element(By.XPATH, '//*[@id="cyrinFrame"]')
+# driver.switch_to.frame(iframe)
 
 
 # issue - waiting for the exercise to finish starting up takes a long time
+# wait for the vm to load
+time.sleep(10)
 
-
+# couldn't find the button
 driver.find_element(By.XPATH, '//*[@id="computersMenuButton"]]').click()
 driver.find_element(By.XPATH, '//*[@id="displaymachine1_Ubuntu2004Desktop-4000-0182-eba4edce-809a-3fe20a37e1aa"]').click()
 
 
-
-# '//*[@id="computersMenuButton"]')).select_by_visible_text('ComputerA')
-
-
-time.sleep(10)
 
 
 
