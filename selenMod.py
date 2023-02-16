71bd8de91ef24390f9933efb37b7b664b710894f
Hoang Che <98788238+hoangche08@users.noreply.github.com>
2023-02-15 20:08:55 -0600

    Update selenMod.py

--- selenMod.py
+++ selenMod.py
@@ -1,90 +1,48 @@
-#Fixed up a bit
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, MoveTargetOutOfBoundsException
+
+
 import time
 
-
-driver = webdriver.Chrome()
        
-def move_and_click(Target):
-    
-    getTargetClick = driver.find_element(By.XPATH, Target)
-    
-    action = ActionChains(driver)
-    
-    action.move_to_element(getTargetClick)\
-        .click()\
-        .perform()
-
-#Some Funny loop
-
-def time_to_wait():
-    
-    while True:       
-            
-        ask_time_wait = input("How many seconds do you want to wait? Enter 0 to exit: ")
-    
-        try:
-                
-            ask_time_wait = int(ask_time_wait)
-            
-            if ask_time_wait == 0:
-                
-                print('Continuing...')
-                break
-                                             
-        except ValueError:
-                
-            print("Invalid input. Please enter a valid integer.")
-    
-        else:
-            
-            print("Time to wait: " + str(ask_time_wait) + " seconds")    
-            time.sleep(ask_time_wait)
-                          
-
-def ask_to_pause():
-         
-    while True:
         
-        ask_to_continues = input("Don you want to pause? (y/n) ").lower()
-    
-        if ask_to_continues == "y":
-                              
-            time_to_wait()
-                     
-        elif ask_to_continues == "n":
-            
-            print('Continuing...')
-            break
-        
-        else:
-            print("invalid input")
-            
-#Funny loop ended here
-            
-            
-#Start forward ask to continues or pause      
-def ask_to_pauseB():
+def pick_option():
     
     while True:
         
-        ask_to_continues = input('Do you wanted to pause (y/n)')
-        
-        if ask_to_continues == "n":
-            
-            break
-        
-        elif ask_to_continues == "y":
-
-            print('Pausing for 10s')
-            time.sleep(10)
-            break
-        
-        else:
-            print('Invalid input')
-            
-            
-        
+        getInputOption = int(input("Press 1 to continue, Press 2 to pause for 5 seconds, Press 3 to hit return button "))
     
+        time.sleep(5)
+        
+        if getInputOption == 1:
+            
+            print('Continuing')
+            break
+        
+        if getInputOption == 2:
+
+            print('Pausing for 5 seconds')
+
+            time.sleep(5)
+            
+            break
+            
+            
+        else :
+            
+            print("Invalid input")
+            
+
+                
+
+
+# Team 2 (two person team): Write a function as a separate Python module.  This function will:
+# Ask the user if they want to: 
+# (1) Continue to the next step, 
+# (2) Pause for a certain number of seconds, 
+# or (2) Move the cursor to a certain location (and then hit return so you know they have done it).
+# Work with Team 3 on what this function should return.
