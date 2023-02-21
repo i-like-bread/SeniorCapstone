31ef3142fca93283c16681e0ce1044a183afb14a
i-like-bread <72410972+i-like-bread@users.noreply.github.com>
2023-02-21 12:06:29 -0800

    group 3 script

--- group3script1.py
+++ group3script1.py
@@ -1,11 +1,4 @@
-import os
-import pytest
-
-# For each line your read write it verbatim to the Enhanced Script file.
-# If the line you read starts with # nn where nn is a number,
-# write this line to the Enhanced Script file.
-# Then write to the file the statement print(prompt_user()).
-# Make sure the line you add is indented correctly.
+import re
 
 # read the golden script
 golden = open("isThisGolden.py", "r")
@@ -16,12 +9,20 @@ outF = open("newScript.py", "a")
 # Manually edit the Enhanced Script to add import team3module to top of script
 outF.write("import selenMod \n")
 
+regex = r"\s+#"
+
 # Read the Golden Script one line at a time.
 for line in golden:
-    outF.write(line)
+    # If the line you read starts with # nn where nn is a number,
+    # write this line to the Enhanced Script file.
+    # Then write to the file the statement print(prompt_user()).
+    if re.match(regex, line):
+        outF.write(line)
+        outF.write("\n    selenMod.pick_option() \n")
+    else:
+        # For each line your read write it verbatim to the Enhanced Script file.
+        outF.write(line)
     outF.write("\n")
-    if line.startswith('#'):
-        outF.write("# tada \n")
 
 outF.close()
 golden.close()
