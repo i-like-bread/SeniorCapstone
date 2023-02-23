fe44ae7d560a2d9b5d8970591e1a3d96ef282ca4
AlexMcMullen <97752475+AlexMcMullen@users.noreply.github.com>
2023-02-23 14:57:02 -0600

    Added calls to run new script
    Added the function calls required to run the new script at the end of the scriptEnhancer function.

--- Script_Editor.py
+++ Script_Editor.py
@@ -10,15 +10,24 @@ def scriptEnhancer():
     # Opens the Golden_Script.py and loops through it
     with open("Golden_Script.py") as script:
         for line in script:
+            
             # if the line is a tabbed in comment that starts with a number, then the Team2 function is called
             if re.match(regex, line):
                 file.write("\n")
                 file.write("    # Prompts the user for next action\n")
                 file.write("    selenMod.pick_option()\n")
                 file.write(line)
+                
             # else, the line is just written to the new file
             else:
                 file.write(line)
-
+                
+    # Insert calls at the end of the Enhanced_Script.py to actually run the new script
+    file.write("\n\n# Running the functions to run the test\n")
+    file.write("testClass = TestCYRINTest()\n\n")
+    file.write('testClass.setup_method("")\n')
+    file.write("testClass.test_cYRINTest()\n")
+    file.write('testClass.teardown_method("")')
+    
 # Runs the function and creates Enhanced_Script.py
 scriptEnhancer()
