a5520b220a3f0fc526c6978d5cca5b745e96b87f
jacvchr <jacvchr@gmail.com>
2023-02-24 03:56:57 -0600

    formatting and comments :D

--- functions.py
+++ functions.py
@@ -4,14 +4,16 @@ import time
 
 
 
-#listen for keyboard input from user; print pressed keys until user hits enter
+#listen for keyboard input from user: enter, w, m, or k
 def promptUser():
     with keyboard.Listener(on_press=on_press_key, on_release=on_release_key) as listener:
         listener.join()
 
+#filler
 def on_press_key(key):
     print("")
 
+#if enter -> continue (end thread), w -> wait, m -> mouseClick, k -> keyboardInput
 #ToDo: Add wait functionality
 def on_release_key(key):
     if(key == keyboard.Key.enter):
@@ -31,13 +33,16 @@ def on_release_key(key):
         print(key,"is invalid, try again")
 
 
+
+#wait for specified amount of time
+#ToDo: Fix
 def waitInput(key):
     print("Wait",key)
     #time.sleep(int(key))
 
 
 
-#get MouseClick coords
+#get MouseClick coords and click
 def mouseClickInput():
     #if user hits enter
     action()
@@ -49,13 +54,17 @@ def mouseClickInput():
     #click at the current position
     pyautogui.click(mousePos)
 
+
+
+#listen to keyboard until user presses enter
 #ToDo: join printed chars together into a string
 def keyboardInput():
     action()
 
 
-#listen until enter key is hit; basically a "continue once user hits enter" method
-#used to allow user to move mouse until they hit enter
+
+#listen until enter key is hit
+#used to know when user is done with action, is nested in other functions
 def action():
     with keyboard.Listener(on_press=on_press, on_release=on_release_enter) as listener:
         listener.join()
