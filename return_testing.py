5cae36ec63e2218b8835ae584d62ea4e43187f9a
AlexMcMullen <amcmulle@cord.edu>
2023-04-07 09:36:40 -0500

    Testing return breakdowns

--- return_testing.py
+++ return_testing.py
@@ -5,3 +5,13 @@ testList = list()
 
 testList.append((getframeinfo(currentframe()).lineno, functions.promptUser()))
 print(testList)
+
+entry = testList[0]
+item = entry[1]
+print(item)
+
+func = item[0]
+print(func)
+
+args = item[1]
+print(args)
