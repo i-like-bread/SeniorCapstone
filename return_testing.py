f6caba943fd314bb4c9af09e10bc352ebba49828
AlexMcMullen <97752475+AlexMcMullen@users.noreply.github.com>
2023-04-12 10:40:26 -0500

    just testing some things out

--- return_testing.py
+++ return_testing.py
@@ -1,17 +1,27 @@
 from inspect import currentframe, getframeinfo
 import functions
+import re
 
-testList = list()
+# testList = list()
 
-testList.append((getframeinfo(currentframe()).lineno, functions.promptUser()))
-print(testList)
+# testList.append((getframeinfo(currentframe()).lineno, functions.promptUser()))
+# print(testList)
 
-entry = testList[0]
-item = entry[1]
-print(item)
+# entry = testList[0]
+# item = entry[1]
+# print(item)
 
-func = item[0]
-print(func)
+# func = item[0]
+# print(func)
 
-args = item[1]
-print(args)
+# args = item[1]
+# print(args)
+
+comment_pattern = "^\s*user_responses\.append\(\(getframeinfo\(currentframe\(\)\)\.lineno, functions\.promptUser\(\)\)\)"
+
+match = re.match(comment_pattern, "     user_responses.append((getframeinfo(currentframe()).lineno, functions.promptUser()))")
+
+if match != None:
+    print("Hell yeah!")
+else:
+    print("Dammit")
